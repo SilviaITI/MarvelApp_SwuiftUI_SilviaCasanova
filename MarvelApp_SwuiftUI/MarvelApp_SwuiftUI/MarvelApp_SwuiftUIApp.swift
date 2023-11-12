f7e0cd58678606eb6c925db78d556aa57fae5cd4
silviaITI <silvia.casanovamartinez@gmail.com>
2023-11-12 17:46:15 +0100

    first commit

---
+++ MarvelApp_SwuiftUI/MarvelApp_SwuiftUI/MarvelApp_SwuiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  MarvelApp_SwuiftUIApp.swift
+//  MarvelApp_SwuiftUI
+//
+//  Created by Silvia Casanova Martinez on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp_SwuiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
